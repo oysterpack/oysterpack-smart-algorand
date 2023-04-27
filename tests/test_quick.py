bb2c2cdf2a02f4d76dc027fefa3a320fbf36035b
oysterpack.inc <2zP6OaymGZhJTHFvQMzp6Ze>
2023-04-27 19:33:30 -0400

    restructured tests package

--- tests/test_quick.py
+++ tests/test_quick.py
@@ -1,9 +1,40 @@
+import asyncio
+import random
 import unittest
+
+from oysterpack.core.ulid import HashableULID
 
 
 class MyTestCase(unittest.TestCase):
     def test_something(self) -> None:
         self.assertEqual(True, True)  # add assertion here
+
+
+class Foo:
+    async def __call__(self) -> int:
+        await asyncio.sleep(0)
+        return random.randint(1, 100)
+
+    @staticmethod
+    def task_name() -> str:
+        return Foo.__qualname__
+
+
+class MyAsyncTestCase(unittest.IsolatedAsyncioTestCase):
+    async def test_task_name(self) -> None:
+        async def foo() -> int:
+            await asyncio.sleep(0)
+            return random.randint(1, 100)
+
+        task = asyncio.create_task(foo())
+        print(task.get_name())
+
+    async def test_task_name_callable(self) -> None:
+        foo = Foo()
+        task = asyncio.create_task(
+            foo(), name=f"{Foo.task_name()}/{str(HashableULID())}"
+        )
+        print(task.get_name())
 
 
 if __name__ == "__main__":
