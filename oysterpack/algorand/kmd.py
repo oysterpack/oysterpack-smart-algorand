436691fdbc5775288a2d16f56db7294beab69042
oysterpack.inc <2zP6OaymGZhJTHFvQMzp6Ze>
2023-05-01 09:00:51 -0400

    new algorand.kmd module - WIP

--- oysterpack/algorand/kmd.py
+++ oysterpack/algorand/kmd.py
@@ -9,6 +9,7 @@ from typing import Any, Self
 from algosdk import kmd
 from password_validator import PasswordValidator
 
+from oysterpack.algorand import Mnemonic
 from oysterpack.core.asyncio.task_manager import schedule_blocking_io_task
 
 
@@ -62,6 +63,19 @@ class KmdService:
 
         return None
 
+    def __validate_wallet_name_password(
+        self, name: str, password: str
+    ) -> tuple[str, str]:
+        name = name.strip()
+        if len(name) == 0:
+            raise ValueError("name cannot be blank")
+        password = password.strip()
+        if len(password) == 0:
+            raise ValueError("password cannot be blank")
+        if self._password_validator and not self._password_validator.validate(password):
+            raise ValueError("password failed validation")
+        return (name, password)
+
     async def create_wallet(self, name: str, password: str) -> Wallet:
         """
         Creates a new wallet using the specified name and password.
@@ -73,18 +87,40 @@ class KmdService:
                          - must have no spaces
         :raises KMDHTTPError:
         """
-        name = name.strip()
-        if len(name) == 0:
-            raise ValueError("name cannot be blank")
-        password = password.strip()
-        if len(password) == 0:
-            raise ValueError("password cannot be blank")
-
-        if self._password_validator and not self._password_validator.validate(password):
-            raise ValueError("password failed validation")
-
+        name, password = self.__validate_wallet_name_password(name, password)
         new_wallet = await schedule_blocking_io_task(
             self._kmd_client.create_wallet, name, password
         )
 
         return Wallet._to_wallet(new_wallet)
+
+    async def recover_wallet(
+        self,
+        name: str,
+        password: str,
+        master_derivation_key: Mnemonic,
+    ) -> Wallet:
+        """
+        Tries to recover a wallet using the specified master derivation key mnemonic.
+        The recovered wallet will be empty. Keys will need to be regenerated.
+
+        Notes
+        -----
+        If a wallet with the same master derivation key already exists but different name already exists, then a new
+        wallet will be created with the specified name and password. Both wallets will generate the same accounts.
+        KMD wallet passwords cannot be changed. If you lost your wallet password, then you can recover your wallet using
+        its master derivation key. If you want to use the same name, then you will need to delete the KMD data directory
+        (or use a new data directory) and start over.
+
+        :raises ValueError: if a wallet with the same name already exists
+        """
+
+        name, password = self.__validate_wallet_name_password(name, password)
+        recovered_wallet = await schedule_blocking_io_task(
+            self._kmd_client.create_wallet,
+            name,
+            password,
+            "sqlite",  # driver_name
+            master_derivation_key.to_kmd_master_derivation_key(),
+        )
+        return Wallet._to_wallet(recovered_wallet)
--- tests/algorand/test_kmd.py
+++ tests/algorand/test_kmd.py
@@ -6,6 +6,7 @@ from beaker import sandbox
 from password_validator import PasswordValidator
 from ulid import ULID
 
+from oysterpack.algorand.keys import AlgoPrivateKey
 from oysterpack.algorand.kmd import KmdService
 
 
@@ -43,7 +44,6 @@ class KmdServiceTestCase(unittest.IsolatedAsyncioTestCase):
         with self.subTest("create new wallet with unique name"):
             name = str(ULID())
             password = str(ULID())
-            print(password)
             wallet = await kmd_service.create_wallet(name=name, password=password)
             self.assertEqual(name, wallet.name)
             wallet_2 = await kmd_service.get_wallet(name)
@@ -112,6 +112,121 @@ class KmdServiceTestCase(unittest.IsolatedAsyncioTestCase):
             password = f"{ULID()}{str(ULID()).lower()}{ULID()}{ULID()}!"
             with self.assertRaises(ValueError) as err:
                 await kmd_service.create_wallet(name=str(ULID()), password=password)
+            self.assertEqual(password_failed_err_msg, str(err.exception))
+
+    async def test_recover_wallet(self):
+        kmd_service = KmdService(
+            url=sandbox.kmd.DEFAULT_KMD_ADDRESS,
+            token=sandbox.kmd.DEFAULT_KMD_TOKEN,
+        )
+
+        with self.subTest("recover wallet using a unique name"):
+            name = str(ULID())
+            password = str(ULID())
+            mdk = AlgoPrivateKey().mnemonic
+
+            wallet = await kmd_service.recover_wallet(
+                name=name,
+                password=password,
+                master_derivation_key=mdk,
+            )
+            self.assertEqual(name, wallet.name)
+
+        with self.subTest("recover wallet again using the same name"):
+            with self.assertRaises(KMDHTTPError) as err:
+                await kmd_service.recover_wallet(
+                    name=name,
+                    password=password,
+                    master_derivation_key=mdk,
+                )
+            err_object = json.loads(str(err.exception))
+            self.assertEqual(
+                "wallet with same name already exists", err_object["message"]
+            )
+
+        with self.subTest("recover wallet again using a different name"):
+            name = str(ULID())
+            wallet = await kmd_service.recover_wallet(
+                name=name,
+                password=password,
+                master_derivation_key=mdk,
+            )
+            self.assertEqual(name, wallet.name)
+
+        with self.subTest("recover wallet with blank name"):
+            name_blank_err_msg = "name cannot be blank"
+            with self.assertRaises(ValueError) as err:
+                await kmd_service.recover_wallet(
+                    name=" ",
+                    password=password,
+                    master_derivation_key=mdk,
+                )
+            self.assertEqual(name_blank_err_msg, str(err.exception))
+
+            with self.assertRaises(ValueError) as err:
+                await kmd_service.create_wallet(name=" ", password=password)
+            self.assertEqual(name_blank_err_msg, str(err.exception))
+
+        with self.subTest("create wallet with blank password"):
+            password_blank_err_msg = "password cannot be blank"
+            with self.assertRaises(ValueError) as err:
+                await kmd_service.recover_wallet(
+                    name=name,
+                    password=" ",
+                    master_derivation_key=mdk,
+                )
+            self.assertEqual(password_blank_err_msg, str(err.exception))
+
+            with self.assertRaises(ValueError) as err:
+                await kmd_service.create_wallet(name=name, password=" ")
+            self.assertEqual(password_blank_err_msg, str(err.exception))
+
+    async def test_wallet_recovery_with_password_validator(self):
+        password_validator = (
+            PasswordValidator()
+            .min(30)
+            .max(80)
+            .uppercase()
+            .lowercase()
+            .symbols()
+            .digits()
+            .has()
+            .no()
+            .spaces()
+        )
+        kmd_service = KmdService(
+            url=sandbox.kmd.DEFAULT_KMD_ADDRESS,
+            token=sandbox.kmd.DEFAULT_KMD_TOKEN,
+            password_validator=password_validator,
+        )
+        mdk = AlgoPrivateKey().mnemonic
+
+        password_failed_err_msg = "password failed validation"
+        with self.subTest("password too short"):
+            with self.assertRaises(ValueError) as err:
+                await kmd_service.recover_wallet(
+                    name=str(ULID()), password="aA1!", master_derivation_key=mdk
+                )
+            self.assertEqual(password_failed_err_msg, str(err.exception))
+
+        with self.subTest("password has spaces"):
+            password = f"{ULID()}{str(ULID()).lower()} !@"
+            with self.assertRaises(ValueError) as err:
+                await kmd_service.recover_wallet(
+                    name=str(ULID()),
+                    password=password,
+                    master_derivation_key=mdk,
+                )
+            self.assertEqual(password_failed_err_msg, str(err.exception))
+
+        with self.subTest("password too long"):
+            password = f"{ULID()}{str(ULID()).lower()}{ULID()}{ULID()}!"
+            with self.assertRaises(ValueError) as err:
+                await kmd_service.recover_wallet(
+                    name=str(ULID()),
+                    password=password,
+                    master_derivation_key=mdk,
+                )
             self.assertEqual(password_failed_err_msg, str(err.exception))
 
 
